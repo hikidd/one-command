f5e5027a35af3ed0e526c5161fa3f24cc9a0b174
Kidd <6661635@qq.com>
2019-12-18 15:34:11 +0800

    Update aliyun_source_1804.sh

--- aliyun_source_1804.sh
+++ aliyun_source_1804.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-sudo echo "deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+echo "deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
 
 deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
@@ -13,4 +13,4 @@ deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe m
 deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
 
 deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse" > /etc/apt/sources.list
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse" | sudo tee /etc/apt/sources.list
