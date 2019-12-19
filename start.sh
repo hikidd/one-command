48a7b62bc79e3104226047b8c698305a0b68cf9b
Kidd <6661635@qq.com>
2019-12-19 10:51:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-sudo apt install net-tools vim terminator git
+sudo apt install -y net-tools vim terminator git mysql-server
 git config --global user.name "kidd.sun"
 git config --global user.email "kidd.sun"
+echo "开始配置mysql..."
+sudo mysql_secure_installation
+echo "请进入mysql执行‘ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';’ 以使root可登录"
