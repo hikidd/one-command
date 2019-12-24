2562575750f6f065a43b0ccbf6658ecac6521fbd
Kidd <6661635@qq.com>
2019-12-24 09:40:02 +0800

    Update RabbitMQ.sh

--- RabbitMQ.sh
+++ RabbitMQ.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #set -x
 
-sudo apt-get install erlang-nox
+sudo apt-get install erlang-nox -y
 sudo apt-get update
-sudo apt-get install rabbitmq-server
+sudo apt-get install rabbitmq-server -y
 sudo rabbitmqctl add_user  admin  admin  
 sudo rabbitmqctl set_user_tags admin administrator 
 sudo rabbitmqctl  set_permissions -p / admin '.*' '.*' '.*'
